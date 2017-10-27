5f3baa2c1105e67e33620c83882acdb3736b257a
Harshal Mhatre <32223134+moreharry@users.noreply.github.com>
2017-10-27 12:16:08 +0530

    Add files via upload

---
+++ EMS_Project_Govind/EMS_Project_Govind/src/com/cg/ems/testcases/ModifyEmployeeTest.java
@@ -0,0 +1,74 @@
+package com.cg.ems.testcases;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.cg.ems.bean.AddressBean;
+import com.cg.ems.bean.DepartmentBean;
+import com.cg.ems.bean.DesignationBean;
+import com.cg.ems.bean.EmployeeBean;
+import com.cg.ems.dao.AdminManagementDAOImpl;
+import com.cg.ems.dao.IAdminManagementDAO;
+import com.cg.ems.exception.EMSException;
+import com.cg.ems.service.AdminManagementServiceImpl;
+import com.cg.ems.service.IAdminManagementService;
+import com.cg.ems.ui.EMSOperation;
+
+public class ModifyEmployeeTest {
+
+	static IAdminManagementService adminServiceRef= null;
+	
+	static EMSOperation emsOp= null;
+	static DepartmentBean deptBean = null;
+	static DesignationBean designBean = null;
+	static EmployeeBean empBean = null;
+	static AddressBean addBean = null;
+	@BeforeClass
+	public static void initialize() {
+		emsOp = new EMSOperation();
+		adminServiceRef = new AdminManagementServiceImpl();
+		deptBean =new DepartmentBean();
+		empBean =new EmployeeBean();
+		addBean = new AddressBean();
+		designBean = new DesignationBean();
+			
+}
+	@Test
+	public void validateObj(){
+		assertNotNull(emsOp);
+		assertNotNull(adminServiceRef);
+		assertNotNull(deptBean);
+		assertNotNull(empBean);
+		assertNotNull(addBean);
+		assertNotNull(designBean);
+		
+		
+	}
+	
+	@Test
+	public void validateUpdate(){
+		
+		try {
+			empBean=adminServiceRef.getEmployeeDetailsBasedOnId(100002);
+			
+			assertNotNull(empBean);
+			
+			//emsOp.updateOptions(empBean);
+			assertSame(1,adminServiceRef.updateFirstName("Aditi",100002));
+			assertSame(1,adminServiceRef.updateLastName("Sanura",100002));	
+			assertSame(1,adminServiceRef.updateMaritalStatus("Married",100002));
+			assertSame(1,adminServiceRef.updateSalary("12000",100002));
+			
+			
+			
+		} catch (EMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	 
+}
---
+++ EMS_Project_Govind/EMS_Project_Govind/src/com/cg/ems/ui/EMSOperation.java
@@ -0,0 +1,654 @@
+package com.cg.ems.ui;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.cg.ems.bean.AddressBean;
+import com.cg.ems.bean.DepartmentBean;
+import com.cg.ems.bean.DesignationBean;
+import com.cg.ems.bean.EmployeeBean;
+import com.cg.ems.exception.EMSException;
+import com.cg.ems.service.AdminManagementServiceImpl;
+import com.cg.ems.service.EmployeeManagementServiceImpl;
+import com.cg.ems.service.IAdminManagementService;
+import com.cg.ems.service.IEmployeeManagementService;
+
+
+public class EMSOperation{
+	
+	Scanner sc = new Scanner(System.in);
+	EmployeeBean employee = new EmployeeBean();
+	IAdminManagementService adminServiceRef = new AdminManagementServiceImpl();
+	IEmployeeManagementService empServiceRef = new EmployeeManagementServiceImpl();
+	ArrayList<EmployeeBean> empBeanList = new ArrayList<EmployeeBean>();
+	
+	
+	
+	void displayEmployeeListDetails(ArrayList<EmployeeBean> empBeanList){
+		
+		System.out.println("ID  || FirstName || LastName  || Department || Grade || Designation \n");
+		
+		for(EmployeeBean beanEmp :empBeanList){
+			//System.out.println(beanEmp);
+			System.out.println(beanEmp.getEmpId()+" || "+beanEmp.getFirstName()+" || "+beanEmp.getLastName()+" || "+beanEmp.getDeptId()+" || "+beanEmp.getGrade()+" || "+beanEmp.getDesignationId());
+		}
+	}
+	
+
+	void displayEmployeeDetails(EmployeeBean beanEmp){
+		System.out.println("ID  || FirstName || LastName  || Department || Grade || Designation \n");
+		System.out.println(beanEmp.getEmpId()+" || "+beanEmp.getFirstName()+" || "+beanEmp.getLastName()+" || "+beanEmp.getDeptId()+" || "+beanEmp.getGrade()+" || "+beanEmp.getDesignationId());
+		
+	}
+	
+	
+	
+	
+	
+	
+	public void showOptionsOfEmployee() throws EMSException {
+		
+		while(true)
+		{
+		System.out.println("Options for Admin");
+		System.out.println("1.Search based on Employee ID");
+		System.out.println("2.Search based on Employee's First name");
+		System.out.println("3.Search based on Employee's Last name");
+		System.out.println("4.Search based on Department ID");
+		System.out.println("5.Search based on Employee's grade");
+		System.out.println("6.Search based on Employee's marital status");
+		System.out.println("7.Get All Employee");
+		System.out.println("8.Exit");
+		System.out.println("What you want :");
+		int choice=sc.nextInt();
+		
+		
+		switch(choice)
+		{
+		case 1:searchBasedOnID();
+			    break;
+		case 2:searchBasedOnFirstName();
+				break;
+		case 3:searchBasedOnLastname();
+				break;
+		case 4:searchBasedOnDepartmentID();
+	    		break;
+		case 5:searchBasedOnGrade();
+				break;
+		case 6:searchBasedOnMaritalStatus();
+				break;
+		case 7:getAllEmployeeDetails();
+		}
+		}
+		
+	}
+
+	/*********************************************** User Operation 
+	 * @throws EMSException **********************************************************/
+	
+	public void searchBasedOnFirstName() throws EMSException {
+		empBeanList = empServiceRef.getEmployeeDetailsBasedOnFirstName("Govind");
+		displayEmployeeListDetails(empBeanList);
+	}
+
+	public void getAllEmployeeDetails() throws EMSException {
+		empBeanList = empServiceRef.getAllEmployeeDetails();
+		displayEmployeeListDetails(empBeanList);
+		
+	}
+
+	public void searchBasedOnMaritalStatus() throws EMSException {
+		empBeanList = empServiceRef.getEmployeeDetailsBasedOnMaritalStatus("Single");
+		displayEmployeeListDetails(empBeanList);
+	}
+
+	public void searchBasedOnGrade() throws EMSException {
+		empBeanList = empServiceRef.getEmployeeDetailsBasedOnGrade("M2");
+		displayEmployeeListDetails(empBeanList);
+		
+	}
+
+	public void searchBasedOnDepartmentID() throws EMSException {
+		empBeanList = empServiceRef.getEmployeeDetailsBasedOnDepartment("Developer");
+		displayEmployeeListDetails(empBeanList);
+		
+	}
+
+	public void searchBasedOnLastname() throws EMSException {
+		empBeanList = empServiceRef.getEmployeeDetailsBasedOnLastName("Prajapati");
+		displayEmployeeListDetails(empBeanList);
+		
+	}
+
+	public void searchBasedOnID() throws EMSException {
+		EmployeeBean empB = empServiceRef.getEmployeeDetailsBasedOnId(10002);
+		displayEmployeeDetails(empB);
+		
+	}
+
+	
+	
+	
+	
+	/*********************************************** Admin Operation 
+	 * @throws EMSException **********************************************************/
+	public void showOptionsOfAdmin() throws EMSException {
+		while(true)
+		{
+		System.out.println("----------:: Welcome Admin ::-------- ");
+		System.out.println("1.Add new Employee");
+		System.out.println("2.Update Employee details");
+		System.out.println("3.View all Employee");
+		System.out.println("4.Search Employee"
+				+ "5.Exit");
+		System.out.println("What you want :");
+		int option=sc.nextInt();
+		
+		switch(option)
+		{
+		case 1:addNewEmployee();
+			    break;
+		case 2:updateEmployeeDetails();
+				break;
+		case 3:viewAllEmployee();
+				break;
+		case 4:searchEmployee();
+				break;
+		case 5:
+			break;
+		}
+		}
+	}
+
+	public void searchEmployee(){
+		System.out.println("\n\nEmployee Details Based on Id\n\n Enter Id Of Employee to Search");
+		int empId = sc.nextInt();
+		
+		EmployeeBean empB = null;
+		try {
+			empB = adminServiceRef.getEmployeeDetailsBasedOnId(empId);
+		} catch (EMSException e) {
+			
+			System.out.println(e.getMessage());
+		
+		}
+		
+		
+		displayEmployeeDetails(empB);
+	}
+
+	public void viewAllEmployee() throws EMSException {
+		
+		/****All Employee Details **/
+		System.out.println("\n\nAll Employee Details");
+		ArrayList<EmployeeBean> empBeanList = adminServiceRef.getAllEmployeeDetails();
+		displayEmployeeListDetails(empBeanList);
+		
+	}
+
+	public void updateEmployeeDetails() throws EMSException {
+
+		System.out.println("\n\nEmployee Details Based on Id\n\n Enter Id Of Employee to Update");
+		int empId = sc.nextInt();
+		
+		EmployeeBean empB = adminServiceRef.getEmployeeDetailsBasedOnId(empId);
+		displayEmployeeDetails(empB);
+		
+		updateOptions(empB);
+	}
+
+	public void updateOptions(EmployeeBean empB) throws EMSException {
+		int empId = empB.getEmpId();
+		
+		System.out.println("in update method "+empB);
+		
+		System.out.println("Option to Update Details "
+				+ "\n1: Update First Name"
+				+ "\n2: Update Last Name"
+				+ "\n3: Update Department Name"
+				+ "\n4: Update Designation"
+				+ "\n5: Update Basic Salary"
+				+ "\n6: Update Marital Status"
+				+ "\n7: Update Home Address"
+				+ "\n8: Update Contact Number\n ::::: Enter Your Choice :::::\n");
+		int choice = sc.nextInt();
+		switch(choice){
+		case 1: {
+				System.out.println("Enter First Name to Update\n");
+				String fName = sc.next();
+				int update = adminServiceRef.updateFirstName(fName,empId);
+				if(update>0){
+					System.out.println("First Name is Updated Successfully");
+				}
+				else
+					System.out.println("Data is not updated successfully...");
+		}
+		break;
+		
+		case 2: {
+			System.out.println("Enter Last Name to Update\n");
+			String lName = sc.next();
+			int update = adminServiceRef.updateLastName(lName,empId);
+			if(update>0){
+				System.out.println("Last Name is Updated Successfully");
+			}
+			else
+				System.out.println("Data is not updated successfully...");
+			
+		}
+		break;
+	
+		case 3:
+		{ 
+			System.out.println("Enter Department Name to Update\n");
+		String dName = sc.next();
+		
+		DepartmentBean deptB = new DepartmentBean();
+		
+		int deptId = empB.getDeptId();
+		
+		deptB.setDeptId(deptId);
+		
+		deptB = adminServiceRef.getDepartmentDetailsById(deptId);
+		System.out.println("Old Department Details "+deptB);
+		
+		
+		int update =adminServiceRef.updateDepartmentName(dName,deptId);
+		
+		if(update>0){
+			System.out.println("Department Name is Updated Successfully");
+			System.out.println("New Department Details "+deptB);
+
+		}
+		else
+			System.out.println("Data is not updated successfully...");
+	}
+	break;
+		case 4: {
+			System.out.println("Enter Designation Name to Update\n");
+			String desigName = sc.next();
+			
+			
+			
+			DesignationBean designB = new DesignationBean();
+			
+			int desigId = empB.getDesignationId();
+			
+			designB.setDesigId(desigId);
+			
+			
+			int update =adminServiceRef.updateDesignationName(desigName,desigId);
+			
+			if(update>0){
+				System.out.println("Designation Name is Updated Successfully");
+			}
+			else
+				System.out.println("Data is not updated successfully...");
+		}
+		break;
+	
+		case 5: {
+			System.out.println("Enter Basic Salary to Update\n");
+			String salary = sc.next();
+			int update =adminServiceRef.updateSalary(salary,empId);
+			if(update>0){
+				System.out.println("Basic Salary is Updated Successfully");
+			}
+			else
+				System.out.println("Data is not updated successfully...");
+		}
+		break;
+		
+		case 6: {
+			System.out.println("Enter Marital Status to Update\n");
+			String marital = sc.next();
+			int update =adminServiceRef.updateMaritalStatus(marital,empId);
+			if(update>0){
+				System.out.println("Marital Status is Updated Successfully");
+			}
+			else
+				System.out.println("Data is not updated successfully...");
+		}
+		break;
+		
+		case 7: {
+			AddressBean bean=new AddressBean();
+			System.out.println("Enter Home Address to Update\n");
+			int addressId = empB.getAddressId();
+			bean.setAddressId(addressId);
+			System.out.println("Enter Door No ::");
+			int doorNo = sc.nextInt();
+			sc.nextLine();
+			bean.setDoorNo(doorNo);
+			System.out.println("Enter Street ::");
+			String street = sc.nextLine();
+			bean.setStreet(street);
+			System.out.println("Enter Landmark ::");
+			String landmark = sc.nextLine();
+			bean.setLandmark(landmark);
+			System.out.println("Enter Area ::");
+			String area =sc.nextLine();
+			bean.setArea(area);
+			System.out.println("Enter Pincode ::");
+			int pincode = sc.nextInt();
+			sc.nextLine();
+			bean.setPincode(pincode);
+			System.out.println("Enter District ::");
+			String district = sc.nextLine();
+			bean.setDistrict(district);
+			System.out.println("Enter State");
+			String state = sc.nextLine();
+			bean.setState(state);
+			AddressBean addr = new AddressBean(addressId,doorNo,street,landmark,area,pincode,district,state);
+		
+			int update =adminServiceRef.updateHomeAddr(addr,addressId);
+			if(update>0){
+				System.out.println("Home Address is Updated Successfully");
+			}
+			else
+				System.out.println("Data is not updated successfully...");
+		}
+		break;
+		
+		
+		case 8: {
+			System.out.println("Enter Contact Number to Update\n");
+			String contactNum = sc.next();
+			int update =adminServiceRef.updateContactNumber(contactNum,empId);
+			if(update>0){
+				System.out.println("Contact Number is Updated Successfully");
+			}
+			else
+				System.out.println("Data is not updated successfully...");
+		}
+		break;
+		
+		}
+		
+		
+		
+	}
+
+	public void addNewEmployee() throws EMSException {
+IAdminManagementService service=new AdminManagementServiceImpl();
+
+		
+		EmployeeBean employee = new EmployeeBean();
+		//System.out.println("Employee ID:");
+		//sc.nextLine();
+		//int empId=sc.nextInt();
+		//employee.setEmpId(empId);
+		//if(service.validateEmpId(empId))
+		//{
+			//if(service.validateEmpId1(empId))
+		//	{
+				
+		System.out.println("\nEnter Employee's first name:");
+		sc.nextLine();
+		String FirstName=sc.nextLine();
+		employee.setFirstName(FirstName);
+
+		if(service.validateFname(FirstName))
+		{
+			
+		if(service.validateFName(FirstName))
+		{
+		
+		
+		System.out.println("\nEnter Employee's last name:");
+		//sc.nextLine();
+		String LastName=sc.nextLine();
+		employee.setLastName(LastName);
+		if(service.validateLname(LastName))
+		{
+		if(service.validateLName(LastName))
+		{
+		
+			
+		
+		System.out.println("\nEnter Employee's date Of joining: dd/MMM/yyyy : ");
+		String df = sc.next();
+		
+		System.out.println("\nEnter Employee's date Of Birth: dd/MMM/yyyy : ");
+		String dt = sc.next();
+		
+	//	try {
+		    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy");
+		    LocalDate datef = LocalDate.parse(df, formatter);
+		    LocalDate datet = LocalDate.parse(dt, formatter); 
+		  //  System.out.println(datef+"  "+datet);
+		   Date df1 = Date.valueOf(datef);
+		    Date dt1 = Date.valueOf(datet);
+		    employee.setDateOfBirth(dt1);
+			employee.setDateOfJoining( df1);
+		    
+			
+		//}
+		
+		
+		/*catch (Exception e) {
+		    System.out.printf("is not parsable!%n");
+		    throw e;      // Rethrow the exception.
+		}*/
+			if(service.validatedate(datef,datet))
+			{
+				
+				
+	long age=service.calculateAge(datet);
+			
+	if(service.validateAge(age))
+			{
+		
+		
+			
+		/*DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+		LocalDate dateOfJoining = LocalDate.parse(d1,formatter);
+		Date doj = Date.valueOf(dateOfJoining);
+		if(service.validateDate(dateOfJoining))
+		{
+		*/
+		
+		/*LocalDate dateOfBirth = LocalDate.parse(d2,formatter);
+		Date dob = Date.valueOf(dateOfBirth);*/
+		
+		
+		System.out.println("Enter Department Name:");
+		String deptName=sc.next();
+		
+		
+		int deptid  = adminServiceRef.isDeptNameExist(deptName);
+		System.out.println(deptid);
+
+		DepartmentBean deptB = new DepartmentBean();
+		deptB.setDeptName(deptName);
+		int deptid1=isDeptNameExist(deptName);
+	
+		
+		int deptID = 0;
+		
+		if(deptid1 > 0)
+		{
+			System.out.println("In If Dept Id"+deptid1);
+			deptB.setDeptId(deptid1);
+			employee.setDeptId(deptid1);
+		}
+		
+		if(deptid1 == 0)
+		{
+		
+			deptB = adminServiceRef.insertDepartment(deptB);
+		
+			if(deptB.getDeptId() >0){
+				deptID =deptB.getDeptId();
+				employee.setDeptId(deptID);
+				System.out.println("Dept Id"+deptID);
+			}
+		}
+		
+		
+		/*DepartmentBean deptB = new DepartmentBean();
+		deptB.setDeptName(deptName);
+		deptB = adminServiceRef.insertDepartment(deptB);
+		
+		int deptID = 0;
+		if(deptB.getDeptId() >0){
+			deptID =deptB.getDeptId();
+		}
+		employee.setDeptId(deptID);
+		
+		System.out.println("Dept Id"+deptID);
+		//System.exit(0);
+		*/
+		
+		System.out.println("Enter Designation Name:");
+		String desigName=sc.next();
+		
+		int desigid  = adminServiceRef.isDesigNameExist(desigName);
+		System.out.println(desigid);
+		
+		DesignationBean desigB = new DesignationBean();
+		desigB.setDesigName(desigName);
+		
+		int desigid1=isDesigNameExist(desigName);
+		int desigId = 0;
+		
+		if(desigid1 >0)
+		{
+			System.out.println("Desig Id"+desigid1);
+			desigB.setDesigId(desigid1);
+			employee.setDesignationId(desigid1);
+		}
+		
+		if(desigid1 == 0)
+		{
+			desigB = adminServiceRef.insertDesignation(desigB);
+			
+			if(desigB.getDesigId() >0)
+			{
+			desigId =desigB.getDesigId();
+			employee.setDesignationId(desigId);
+			System.out.println("Desig Id"+desigId);
+		
+		}
+			
+		}
+		
+		/*System.out.println("Enter Department ID:");
+		int deptId=sc.nextInt();
+		employee.setDeptId(deptId);*/
+		
+		/*System.out.println("Enter Designation ID:");
+		int designationId=sc.nextInt();
+		employee.setDesignationId(designationId);*/
+		
+		System.out.println("\nEnter Employee's Salary:");
+		Long salary=sc.nextLong();
+		employee.setSalary(salary);
+		
+		String grade=service.obtaingrade(salary);
+	
+			
+		employee.setGrade(grade);
+		System.out.println(grade);
+		System.out.println("\nEnter Gender:");
+		String gender=sc.next();
+		employee.setGender(gender);
+		
+		System.out.println("\nEnter martial Status:");
+		String martialStatus=sc.next();
+		employee.setMartialStatus(martialStatus);
+		
+		
+		
+		AddressBean addB = new AddressBean();
+		System.out.println("Enter door number:");
+		int doorNo=sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println("Enter street:");
+		String street=sc.nextLine();
+		
+		System.out.println("Enter landmark:");
+		String landmark=sc.nextLine();
+		
+		System.out.println("Enter Area:");
+		String area=sc.nextLine();
+		
+		System.out.println("Enter pincode:");
+		int pincode=sc.nextInt();
+		
+		sc.nextLine();
+		System.out.println("Enter district:");
+		String district=sc.nextLine();
+		
+		System.out.println("Enter state:");
+		String state=sc.nextLine();
+		
+		addB.setDoorNo(doorNo);
+		addB.setStreet(street);
+		addB.setLandmark(landmark);
+		addB.setArea(area);
+		addB.setPincode(pincode);
+		addB.setDistrict(district);
+		addB.setState(state);
+		
+		addB = adminServiceRef.insertAddress(addB);
+		System.out.println("Address Id is "+addB.getAddressId());
+		
+		
+		int addId =0;
+		if(addB.getAddressId()>0){
+			addId =addB.getAddressId();
+		}
+		
+		employee.setAddressId(addId);
+		
+		
+		//System.out.println("Enter Address ID:");
+		//int addressId=sc.nextInt();
+		//employee.setAddressId(addressId);
+	
+		System.out.println("Enter Contact number:");
+		long contactNo=sc.nextLong();
+		employee.setContactNo(contactNo);	
+		
+		
+		
+		// add Employee details
+
+		//EmployeeBean employee = new EmployeeBean(empId,FirstName,LastName,df1,dt1,deptId,designationId,"M2",gender,salary,martialStatus,addressId,contactNo);
+
+		//System.out.println(employee);
+		System.out.println(employee);
+		
+	//	employee.setEmpId(empId);
+		EmployeeBean emp= adminServiceRef.addEmployeeDetails(employee);
+		System.out.println(emp);
+		displayEmployeeDetails(emp);
+
+		
+			}
+		}
+		}
+		}
+		}
+		}
+			}
+
+
+	private int isDesigNameExist(String desigName) {
+		return adminServiceRef.isDesigNameExist(desigName);
+	}
+
+
+	private int isDeptNameExist(String deptName) {
+		return adminServiceRef.isDeptNameExist(deptName);
+	}
+		
+	}
+
+	
+
